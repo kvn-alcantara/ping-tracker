0f7bf3ab675ec1cdb7fa48f3c42755757ab05f1c
Kevin Alcantara <42805770+kvn-alcantara@users.noreply.github.com>
2025-04-07 15:31:09 -0300

    add file logger implementation

--- internal/delivery/cli/logger.go
+++ internal/delivery/cli/logger.go
@@ -10,6 +10,29 @@ type Logger interface {
 }
 
 type FileLogger struct {
-	logFile *os.File
-	logger  *log.Logger
+	file   *os.File
+	logger *log.Logger
 }
+
+func NewFileLogger(filePath string) (*FileLogger, error) {
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &FileLogger{
+		file:   file,
+		logger: log.New(file, "[HOST-MONITOR] ", log.LstdFlags),
+	}, nil
+}
+
+func (l *FileLogger) Log(message string) {
+	l.logger.Println(message)
+}
+
+func (l *FileLogger) Close() {
+	if l.file != nil {
+		l.file.Close()
+	}
+}
+
+var _ Logger = (*FileLogger)(nil)
