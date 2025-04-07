115df35d388e2e7880f117b27ed3f70f5aab97c0
Kevin Alcantara <kevin.alcantara@proton.me>
2025-04-07 15:18:42 -0300

    add file logger implementation

--- cmd/pingtracker/main.go
+++ cmd/pingtracker/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/kvn-alcantara/ping-tracker/internal/delivery/cli"
+	"github.com/kvn-alcantara/ping-tracker/internal/repository"
 )
 
 func main() {
-	display := cli.NewTerminalDisplay()
+	logger, err := cli.NewFileLogger("host_monitor.log")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating logger: %v\n", err)
+		os.Exit(1)
+	}
+	defer logger.Close()
 
-	display.PrintHeader("Ping Tracker")
+	cli.NewTerminalDisplay()
+	repository.NewProBingPinger()
+
+	logger.Log("Ping Tracker started")
 }
---
+++ internal/delivery/cli/logger_test.go
@@ -0,0 +1,52 @@
+package cli
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewFileLogger(t *testing.T) {
+	withTempFile(t, func(tempFile string) {
+		logger, err := NewFileLogger(tempFile)
+		assert.NoError(t, err, "expected no error when creating a new FileLogger")
+		assert.NotNil(t, logger, "expected logger to be initialized")
+
+		_, err = os.Stat(tempFile)
+		assert.NoError(t, err, "expected log file to be created")
+	})
+}
+
+func TestFileLoggerLog(t *testing.T) {
+	withTempFile(t, func(tempFile string) {
+		logger, err := NewFileLogger(tempFile)
+		assert.NoError(t, err, "expected no error when creating a new FileLogger")
+		defer logger.Close()
+
+		testMessage := "This is a test log message"
+		logger.Log(testMessage)
+
+		content, err := os.ReadFile(tempFile)
+		assert.NoError(t, err, "expected no error when reading the log file")
+		assert.Contains(t, string(content), testMessage, "expected log file to contain the test message")
+	})
+}
+
+func TestFileLoggerClose(t *testing.T) {
+	withTempFile(t, func(tempFile string) {
+		logger, err := NewFileLogger(tempFile)
+		assert.NoError(t, err, "expected no error when creating a new FileLogger")
+
+		logger.Close()
+
+		_, err = os.Stat(tempFile)
+		assert.NoError(t, err, "expected log file to still exist after closing the logger")
+	})
+}
+
+func withTempFile(t *testing.T, testFunc func(tempFile string)) {
+	tempFile := "test_logger.log"
+	defer os.Remove(tempFile)
+	testFunc(tempFile)
+}
